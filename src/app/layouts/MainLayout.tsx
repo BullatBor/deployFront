085effea175faf43a91d1dbff2c3874060c3aa2b
danya <lovec_omarov@mail.ru>
2024-07-17 00:09:22 +0300

    feat(shared): MSD-14 Реализовал ScrollUpBtn

--- src/app/layouts/MainLayout.tsx
+++ src/app/layouts/MainLayout.tsx
@@ -1,13 +1,33 @@
 import { Outlet } from 'react-router-dom';
 import { Header } from '@/widgets/header';
 import { Footer } from '@/widgets/footer';
+import { useEffect, useState } from 'react';
+import { ScrollUpBtn } from '@/shared';
 
 export const MainLayout = () => {
+  const [scrolled, setScrolled] = useState<boolean>(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const offset = window.scrollY;
+      if (offset > 1000) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
   return (
     <div>
       <Header />
       <Outlet />
       <Footer />
+      {scrolled && <ScrollUpBtn />}
     </div>
   );
 };
--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -2,4 +2,5 @@ export * from '../../widgets/headerLinks';
 export * from './languageButtons';
 export * from './button';
 export * from './icon';
+export * from './scrollUpBtn';
 export * from './text';
