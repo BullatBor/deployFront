0081b64019979572f54be543a74ae2f200b661e4
danya <lovec_omarov@mail.ru>
2024-07-18 11:34:09 +0300

    feat(widgets): MSD-20 Реализовал AboutTree

--- src/widgets/aboutTree/AboutTree.tsx
+++ src/widgets/aboutTree/AboutTree.tsx
@@ -1,5 +1,25 @@
-import React from 'react';
+import styles from './AboutTree.module.scss';
+import { Text } from '../../shared';
+import { useTranslation } from 'react-i18next';
 
 export const AboutTree = () => {
-  return <div>AboutTree</div>;
+  const { t } = useTranslation();
+
+  return (
+    <div className={styles['tree']}>
+      <div className={styles['tree__wrapper']}>
+        <div className={styles['tree__heading']}>
+          <Text tag='h5' weight='regular'>
+            {'c.'}
+          </Text>
+          <Text tag='h1' weight='regular'>
+            {t('aboutTree.heading')}
+          </Text>
+        </div>
+        <div className={styles['tree__image']}>
+          <img src='src/shared/ui/icon/assets/images/academic_tree.png' alt='tree' />
+        </div>
+      </div>
+    </div>
+  );
 };
