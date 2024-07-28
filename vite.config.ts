1025320f8df7040c6b086c257a0665a180c52ab8
danya <lovec_omarov@mail.ru>
2024-07-28 23:34:08 +0300

    feat(root): MSD-32 Настроил тестовое окружение

--- vite.config.ts
+++ vite.config.ts
@@ -18,6 +18,9 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
+  optimizeDeps: {
+    exclude: ['jest', 'testing-library'],
+  },
   plugins: [
     react(),
     vitePluginSvgr({
